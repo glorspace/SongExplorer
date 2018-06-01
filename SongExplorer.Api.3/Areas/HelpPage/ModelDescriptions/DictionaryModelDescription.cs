74a672509f84ea1f1a002a0df24cab074a3b8eea
glorspace <tommysims@gmail.com>
2018-06-01 10:20:38 -0400

    Deleted old Api.3 project.

--- SongExplorer.Api.3/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace SongExplorer.Api._3.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- SongExplorer.Api.3/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SongExplorer.Api._3.WebApiApplication" Language="C#" %>
